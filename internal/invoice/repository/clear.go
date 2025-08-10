a214d4d8b8723dcdfa39db95449bf8caf3bd8724
donatomartin <uo288787@uniovi.es>
2025-08-10 13:40:03 +0200

    fix: correct clear invoices query

--- internal/invoice/repository/clear.go
+++ internal/invoice/repository/clear.go
@@ -3,6 +3,6 @@ package repository
 import "context"
 
 func (r *InvoiceRepository) Clear(ctx context.Context) error {
-	_, err := r.db.ExecContext(ctx, "DELETE FROM payments")
+	_, err := r.db.ExecContext(ctx, "DELETE FROM invoices")
 	return err
 }
